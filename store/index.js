3ca93cd20118f026fbd9db12510a707b2f0f59f9
Amirerfan <amirerfan.siamaky@gmail.com>
2019-10-10 15:25:43 -0400

    Login Logic and Loading Button

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+
+export const state = () => ({
+    loading: false,
+ 
+})
+  
+export const mutations = {
+    toggleLoading (state, flag) {state.loading = flag},
+}
+
+export const actions = {
+
+}
