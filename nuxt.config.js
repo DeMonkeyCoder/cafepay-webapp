84e1bc759a10c549c94a1c73b588f23e3ea6a253
beigiz <beigi.work@gmail.com>
2020-02-29 17:25:37 +0330

    fix negative margin for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 import webpack from 'webpack'
 export default {
    server: {     
-    port: 3000, // default: 3000     
+    // port: 3000, // default: 3000     
     host: '0.0.0.0', // default: localhost   
   }, 
   mode: 'spa',
