590502226e75fc83c62a17ea01bc5c35ecc08902
beigiz <beigi.work@gmail.com>
2020-03-21 12:51:20 +0430

    cafe comment in progress

---
+++ components/cafe/featureStatic.vue
@@ -0,0 +1,30 @@
+<template>
+      <div class="cp-side-padding cp-tb-padding" 
+     > 
+      <div class="cp-card cp-side-padding cp-tb-padding long-shadow">
+        <div class="feature-icon">
+          <slot></slot>
+        </div>
+        
+      <p class="feature-rate">{{feature.rate}}</p>
+      <p class="feature-rate-count">{{feature.count}}</p>
+      </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+ 
+      feature: {
+        default: {}
+      },
+
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
