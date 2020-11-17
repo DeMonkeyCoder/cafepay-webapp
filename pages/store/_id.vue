3bfc7702cf110ac3897c70181928f18f4fe055a0
beigiz <beigi.work@gmail.com>
2020-11-17 17:48:55 +0330

    menu-only-test -- img inclue

---
+++ pages/store/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    middleware({params, redirect}){
+      console.log('prarams', params, redirect);
+      redirect(`/user/home/?token=${params.id}`)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
