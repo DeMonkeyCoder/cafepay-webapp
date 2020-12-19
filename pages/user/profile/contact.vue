660a11897c812b2151b8c9aa45e7b07d96ed4493
beigiz <beigi.work@gmail.com>
2020-12-19 15:59:14 +0330

    add contact us section

--- pages/user/profile/contact.vue
+++ pages/user/profile/contact.vue
@@ -1,7 +1,5 @@
 <template>
   <div :dir="$dir()">
-
-
     <div class="cp-header cp-tb-padding cp-side-padding">
       <div @click="$router.go(-1)" class="go-back cp-tb-padding">
         <b-icon size="is-medium" icon="chevron-left" type="is-light"></b-icon>
@@ -12,11 +10,7 @@
       class="profile-info-bar cp-side-padding cp-tb-padding long-shadow cp-side-margin cp-header-card has-background-white"
     >
       <div class="info">
-        <img
-          class="no-pic"
-          src="@/assets/img/shape/icons/contact.svg"
-          alt
-        />
+        <img class="no-pic" src="@/assets/img/shape/icons/contact.svg" alt />
         <h4 class="header cp-tb-padding cp-side-padding">تماس با ما</h4>
         <p :dir="$dir()" class="detail cp-tb-padding cp-side-padding">
           با کافه‌پی در ارتباط باشید!
@@ -25,34 +19,50 @@
       </div>
     </div>
 
-    <section class="content-below-profile-bar contact cp-side-margin">
-      <div class="contact__card">
-        <p>مشاوره و فروش</p>
-        <img src="@/assets/img/shape/icons/con-sale.svg" alt="">
+    <section class="content-below-profile-bar profile-contact cp-side-margin">
+      <p class="profile-contact__guide cp-side-padding ">جهت دریافت مشاوره رایگان در خصوص سرویس‌های کافه‌پِی، کسب اطلاعات بیشتر و ثبت پیشنهادات و انتقادات با شماره‌های زیر تماس بگیرید</p>
+
+      <img class="profile-contact__arrow" src="@/assets/img/shape/arrow.png" alt="">
+
+      <a href="tel:0930-313-1503"  data-rel="external">
+      <div class="profile-contact__card cp-side-padding cp-b-margin cp-tb-padding-2x">
+        <img src="@/assets/img/shape/icons/con-sale.svg" alt="" />
+        <div class="profile-contact__card__text cp-side-padding">
+          <p class="profile-contact__card__text__role font-16 font-bold">مشاوره و فروش</p>
+          <p dir="ltr" class="profile-contact__card__text__number font-18">0930 313 1503</p>
+        </div>
+        <div class="profile-contact__card__dialer">
+          <b-icon icon="phone"></b-icon>
+        </div>
       </div>
-          <div class="contact__card">
-            <p>پشتیبانی</p>
-            <img src="@/assets/img/shape/icons/con-support.svg" alt="">
-          </div>
+      </a>
+
+      <a href="tel:0917-116-4374"  data-rel="external">
+      <div class="profile-contact__card cp-side-padding cp-tb-padding-2x">
+        <img src="@/assets/img/shape/icons/con-support.svg" alt="" />
+        <div class="profile-contact__card__text cp-side-padding">
+          <p class="profile-contact__card__text__role font-16 font-bold">پشتیبانی کافه‌پی</p>
+          <p dir="ltr" class="profile-contact__card__text__number font-18">0917 116 4374</p>
+        </div>
+        <div class="profile-contact__card__dialer">
+          <b-icon icon="phone"></b-icon>
+        </div>
+      </div>
+      </a>
+
     </section>
   </div>
 </template>
 
 <script>
-
 export default {
-
-  mounted() {}
+  mounted() {},
 }
 </script>
 
 <style scoped lang="sass">
 // Normalize/Reset only elements used
 
-
 // Mixin for shadow button with block sliding up
   // $easing: cubic-bezier(0.4, 0, 0.2, 1)
-
-
-
 </style>
