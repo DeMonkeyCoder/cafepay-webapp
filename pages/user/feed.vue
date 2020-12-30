3bc5c65597cc101e67b7baa03babcf6b8b113190
beigiz <beigi.work@gmail.com>
2020-12-31 01:55:12 +0330

    adding tags in list

--- pages/user/feed.vue
+++ pages/user/feed.vue
@@ -2,26 +2,35 @@
   <div dir="rtl">
     <div class="feed">
       <img class="feed__header" src="@/assets/img/feed-shapes-1.png" alt="" />
-              <b-input
-              ref="tablecode"
-              class="cp-input feed__search cp-input-primary  shadow-lg"
-              type="code"
-              icon="magnify"
-              placeholder="جست‌وجو در کافه‌پِی"
-            ></b-input>
-          </b-field>
+      <b-field>
+        <b-input
+          ref="tablecode"
+          class="cp-input feed__search cp-input-primary shadow-lg"
+          type="code"
+          icon="magnify"
+          placeholder="جست‌وجو در کافه‌پِی"
+        ></b-input>
+      </b-field>
+
       <section class="feed__post">
         <header class="feed__section-header">رویدادها</header>
         <div class="feed__post__wrapper">
-        <div class="feed__post__list" v-for="(post, index) in postList" :key="index">
-          <img class="feed__post__list__avatar" :src="post.img" alt="">
-          <img class="feed__post__list__img" :src="post.avatar" alt="">
-          <div class="feed__post__list__content shadow-lg">
-            <p class="eed__post__list__content__text">{{post.short_description}}</p>
+          <div
+            class="feed__post__list"
+            v-for="(post, index) in postList"
+            :key="index"
+          >
+            <img class="feed__post__list__avatar" :src="post.img" alt="" />
+            <img class="feed__post__list__img" :src="post.avatar" alt="" />
+            <div class="feed__post__list__content shadow-lg">
+              <p class="eed__post__list__content__text">
+                {{ post.short_description }}
+              </p>
+            </div>
           </div>
         </div>
-        </div>
       </section>
+      
       <section class="feed__cafe-list">
         <header class="feed__section-header">کافه و رستوران</header>
         <div
@@ -29,14 +38,19 @@
           :key="index"
           class="feed__cafe-list__cafe"
           :style="{
-            backgroundImage: `linear-gradient(to right, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 1) 85%),url(${cafe.header})`
+            backgroundImage: `linear-gradient(to right, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.85) 85%),url(${cafe.header})`,
           }"
         >
           <img class="shadow" :src="cafe.img" alt="" />
           <div class="cafe-content">
             <p class="cafe-content__name">{{ cafe.name }}</p>
             <p class="cafe-content__short-desc">{{ cafe.description }}</p>
+            <div class="cafe-content__tags">
+              <span class="cp-side-padding-half font-12 shadow-lg">پیکاپ</span>
+              <span class="cp-side-padding-half font-12 shadow-lg">حضوری</span>
+            </div>
           </div>
+        
         </div>
       </section>
     </div>
@@ -55,18 +69,21 @@ export default {
         {
           img: require('~/assets/img/shape/icons/test-cafe.png'),
           avatar: require('~/assets/img/feed-post-img.jpg'),
-          short_description: ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،'
+          short_description:
+            ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،',
         },
-            {
+        {
           img: require('~/assets/img/shape/icons/test-cafe.png'),
           avatar: require('~/assets/img/feed-post-img.jpg'),
-          short_description: ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،'
+          short_description:
+            ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،',
         },
-            {
+        {
           img: require('~/assets/img/shape/icons/test-cafe.png'),
           avatar: require('~/assets/img/feed-post-img.jpg'),
-          short_description: ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،'
-        }
+          short_description:
+            ' لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،',
+        },
       ],
       cafeList: [
         {
@@ -74,47 +91,47 @@ export default {
           token: '123456',
           description: 'توضیح مختصر در این قسمت',
           img: require('~/assets/img/shape/icons/test-cafe.png'),
-          header: require('~/assets/img/1.jpg')
+          header: require('~/assets/img/1.jpg'),
         },
         {
           name: 'باغشاه - ۵۰',
           token: '123456',
           description: 'توضیح مختصر در این قسمت',
           img: require('~/assets/img/shape/icons/test-cafe.png'),
-          header: require('~/assets/img/2.jpg')
+          header: require('~/assets/img/2.jpg'),
         },
         {
           name: 'باغشاه - ۵۰',
           token: '123456',
           description: 'توضیح مختصر در این قسمت',
           img: require('~/assets/img/shape/icons/test-cafe.png'),
-          header: require('~/assets/img/3.jpg')
+          header: require('~/assets/img/3.jpg'),
         },
         {
           name: 'باغشاه - ۵۰',
           token: '123456',
           description: 'توضیح مختصر در این قسمت',
           img: require('~/assets/img/shape/icons/test-cafe.png'),
-          header: require('~/assets/img/4.jpg')
-        }
-      ]
+          header: require('~/assets/img/4.jpg'),
+        },
+      ],
     }
   },
   methods: {},
 
   mounted() {
     this.$store.dispatch('cafe/retreiveList')
-  //   this.$bus.$on('shrink', flag => {
-  //     if (flag) {
-  //       $('.navigation-in-home').addClass('navigation-in-home-shrink')
-  //     } else {
-  //       $('.navigation-in-home').removeClass('navigation-in-home-shrink')
-  //     }
-  //   })
+    //   this.$bus.$on('shrink', flag => {
+    //     if (flag) {
+    //       $('.navigation-in-home').addClass('navigation-in-home-shrink')
+    //     } else {
+    //       $('.navigation-in-home').removeClass('navigation-in-home-shrink')
+    //     }
+    //   })
   },
   computed: {
     list() {
-      return this.$store.state.cafe.list 
+      return this.$store.state.cafe.list
     },
   },
 }
