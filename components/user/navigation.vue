47fa700d176c959fcaea263edd6b20321a58f662
beigiz <beigi.work@gmail.com>
2020-12-30 15:49:27 +0330

    add files

--- components/user/navigation.vue
+++ components/user/navigation.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="navigation-container">
+
+      
+    <nuxt-link to="/user/feed" class="nav-tab home-icon center-align" :class="{'is-active': $route.name == 'user-feed'}">
+      <img v-show="$route.name == 'user-feed'" src='@/assets/img/shape/icons/home-5.png' alt="">
+      <img v-show="$route.name != 'user-feed'" src='@/assets/img/shape/icons/home.png' alt="">
+    </nuxt-link>
    
-    <div @click="triggerChangeTab((hasActiveTable) ? 'currentCafe' : 'scan')" class="nav-tab center-align" 
+    <div @click="triggerChangeTab((hasActiveTable) ? 'currentCafe' : 'scan')" class="nav-tab qr-scan center-align" 
     :class="{'is-active': currentMainPage == 'scan' || currentMainPage == 'currentCafe'}">
-      <img v-if="currentMainPage == 'scan' || currentMainPage == 'currentCafe'" src='@/assets/img/shape/icons/qr-code-color.png' alt="">
-      <img v-else src='@/assets/img/shape/icons/qr-code.png' alt="">
-      <!-- <p>اسکن</p> -->
+
+      <img v-show="$route.name != 'user-feed' && currentMainPage == 'scan' && !tableToken" src='@/assets/img/shape/icons/qr-code-color.png' alt="">
+      <img v-show="($route.name == 'user-feed' && !hasActiveTable) || currentMainPage != 'scan' && !tableToken" src='@/assets/img/shape/icons/qr-code.png' alt="">
+
+      <img v-show="$route.name != 'user-feed' && currentMainPage == 'currentCafe' && tableToken" src='@/assets/img/shape/icons/menu-color-5.png' alt="">
+      <img  v-show="($route.name == 'user-feed' && hasActiveTable) || currentMainPage != 'currentCafe' && tableToken" src='@/assets/img/shape/icons/menu.png' alt="">
+      <!-- <p v-show="$route.name != 'user-feed' && !tableToken">اسکن</p>
+      <p v-show="$route.name != 'user-feed' && tableToken">منو</p> -->
     </div>
 
     <!-- <div @click="triggerChangeTab('search')" class="nav-tab center-align" :class="{'is-active': currentMainPage == 'search'}">
@@ -14,15 +25,14 @@
     </div> -->
 
     <div @click="triggerChangeTab('cp-table')" class="nav-tab chair center-align" :class="{'is-active': currentMainPage == 'cp-table'}">
-      <img v-if="currentMainPage == 'cp-table'" src='@/assets/img/shape/icons/chair-color.png' alt="">
-      <img v-else src='@/assets/img/shape/icons/chair.png' alt="">
-      <span v-if="user.table_uuid" class="notif-num has-background-danger"></span>
+      <img v-show="$route.name != 'user-feed' && currentMainPage == 'cp-table'" src='@/assets/img/shape/icons/chair-color.png' alt="">
+      <img v-show="$route.name == 'user-feed' || currentMainPage != 'cp-table'" src='@/assets/img/shape/icons/chair-1.png' alt="">
       <!-- <p>میز</p> -->
     </div>
 
     <div @click="triggerChangeTab('profile')" class="nav-tab profile center-align" :class="{'is-active': currentMainPage == 'profile'}">
-      <img v-if="currentMainPage == 'profile'" src='@/assets/img/shape/icons/user-color.svg' alt="">
-      <img v-else src='@/assets/img/shape/icons/user.png' alt="">
+      <img v-show="$route.name != 'user-feed' && currentMainPage == 'profile'" src='@/assets/img/shape/icons/user-color.svg' alt="">
+      <img v-show="$route.name == 'user-feed' || currentMainPage != 'profile'" src='@/assets/img/shape/icons/user.png' alt="">
       <!-- <span class="notif-num has-background-danger">1</span> -->
       <!-- <p>پروفایل</p> -->
     </div>
@@ -40,6 +50,7 @@
       triggerChangeTab(componentName) {
         this.tabName = componentName
         this.$store.commit('changeNavigation', componentName)
+        if (this.$route.name == "user-feed") this.$router.push('/user/home')
         
         // this.$emit('changeTab',componentName)
       },
@@ -62,8 +73,8 @@
   opacity: 1!important
 
 .navigation-container
+  transition: 0.5s
   z-index: 105
-  // border-radius: 1rem 1rem 0 0
   border-radius: 25px 25px 0 0
   position: fixed
   display: flex
@@ -81,19 +92,7 @@
       height: 23px
       position: absolute
       left: calc(50% - 30px)
-      top: calc(50% - 5px)
-  .nav-tab.chair
-    position: relative
-    .notif-num
-      animation-name: blink
-      animation-duration: 2s
-      animation-timing-function: ease
-      animation-iteration-count: infinite
-      width: 20px
-      height: 20px
-      position: absolute
-      left: calc(50% - 40px)
-      top: calc(50% - 5px)
+      top: calc(50% - 15px)
 
   .nav-tab
     padding-top: 15px
@@ -111,17 +110,34 @@
       font-weight: 500!important
       color: $primary
 
-  .chair
+  .home-icon
     img
-      width: 45px!important
+      width: 34px!important
+      height: 34px!important
+
+  .qr-scan
+    img
+      width: 32px!important
       position: relative
-      bottom: 6px
+      top: 1px
+  .table-icon
+    img
+      width: 35px!important
+      height: 35px!important
+
+  .chair
+    img 
+      // width: 40px !important
+      width: 48px!important
+      height: 52px
+      position: relative
+      bottom: 8px
     p
       bottom: 19px!important
 
   .profile
     img
-      width: 28px!important
+      // width: 28px!important
       position: relative
       top: 2px
     p
