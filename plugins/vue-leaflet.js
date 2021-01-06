7c2e188389b82b2a5369a669006d69659fa1c63a
beigiz <beigi.work@gmail.com>
2021-01-06 05:02:35 +0330

    fix map

--- plugins/vue-leaflet.js
+++ plugins/vue-leaflet.js
@@ -6,8 +6,6 @@ import {
   LTileLayer
 } from 'vue2-leaflet'
 
-if (process.client){
-
 const VueLeaflet = {
 
   install(Vue, options) {
@@ -19,6 +17,5 @@ const VueLeaflet = {
 };
 
 Vue.use(VueLeaflet);
-}
 
 export default VueLeaflet;
